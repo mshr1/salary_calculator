51cb627478f161ba2dbc9f28d8caf36916edf1fc
Sergey <ser.chachillo@bk.ru>
2022-11-07 17:27:30 +0300

    make function salary, 1 step

--- js/main.js
+++ js/main.js
@@ -4,14 +4,27 @@
 // Принимает на вход параметры, позиции и килограммы,
 // из которых складывает все это в одну сумму и выводит на экран
 
-let positionsBasic = document.querySelector('.input-position'),
-  weightBasic = document.querySelector('.input-weight'),
+let positions = document.querySelector('.input-position'),
+  weight = document.querySelector('.input-weight'),
+  select = document.querySelector('#worker'),
   btn = document.querySelector('.btn-addition'),
   textOut = document.querySelector('.text-output');
 
-btn.onclick = function () {
-  textOut.innerHTML = parseInt(positionsBasic.value) * 4 + parseInt(weightBasic.value) * 0.22;
+btn.onclick = function (event) {
+  event.preventDefault();
+
+  // Controller
+
+  if (positions.value == null) {
+    textOut.innerHTML = weight.value * 0.22;
+  } else if (weight.value == null) {
+    textOut.innerHTML = positions.value * 4;
+  } else {
+    textOut.innerHTML = positions.value * 4 + weight.value * 0.22;
+  }
 }
+
+
 
 
 
